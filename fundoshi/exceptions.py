793bc6d8bfd5fdd3611e79dbd75b436f16deb35c
nhanb <nhan@nerdyweekly.com>
2015-06-09 13:55:50 +0700

    handle unsupported site error

---
+++ fundoshi/exceptions.py
@@ -0,0 +1,2 @@
+class UnsupportedSiteError(Exception):
+    pass
--- fundoshi/main.py
+++ fundoshi/main.py
@@ -1,7 +1,10 @@
 from .sites import get_site
+from .exceptions import UnsupportedSiteError
 
 
 def parse_chapter(url):
     site = get_site(url)
+    if site is None:
+        raise UnsupportedSiteError()
     resp = site.get_chapter_seed_page(url)
     return site.chapter_info(resp.text)
